179c82cdc012a3583f60f0f27db06e24f2c014be
bEERy <siradanai27@gmail.com>
2020-10-02 15:44:58 +0700

    Add Stack in Python

---
+++ Python/stack.py
@@ -0,0 +1,15 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def pop(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack.pop()
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def size(self):
+        return len(self.stack)
