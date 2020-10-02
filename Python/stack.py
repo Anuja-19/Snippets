11da39b3162cf0d00778f0653fe2a2113d8d2e19
bEERy <siradanai27@gmail.com>
2020-10-02 18:22:33 +0700

    Add Example for stack

--- Python/stack.py
+++ Python/stack.py
@@ -13,3 +13,34 @@ class Stack:
 
     def size(self):
         return len(self.stack)
+
+
+# Example
+stc = Stack()
+stc.push(1)
+stc.push(2)
+stc.push(3)
+stc.push(4)
+stc.push(5)
+stc.push(6)
+stc.push(7)
+print("Stack is : " + str(stc.stack))
+print("Stack size : " + str(stc.size()))
+print("----------------------------------")
+stc.pop()
+stc.pop()
+stc.pop()
+print("Stack after pop is : " + str(stc.stack))
+print("Stack size : " + str(stc.size()))
+
+
+"""
+
+The output of the above program will be:
+Stack is : [1, 2, 3, 4, 5, 6, 7]
+Stack size : 7
+----------------------------------
+Stack after pop is : [1, 2, 3, 4]
+Stack size : 4
+
+"""
