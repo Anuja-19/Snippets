46c6a31679f4b73d6bdbd8169c360eab6ad3eef0
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-10-04 10:49:44 +0530

    added kadane in python and java

--- Python/Kadane_Algorithm.py
+++ Python/Kadane_Algorithm.py
@@ -2,7 +2,7 @@
 Kadane's Algorithm:
 The maximum sum subarray problem is the task of finding a contiguous subarray with the largest sum, within a given one-dimensional array A[1...n] of numbers.
 This is one of the most popular problems and is asked in almost all of the Technical Interviews of top Companies.
-Kadne's algorithm can be used to find the sum of the largest sum contiguous subarray or modified to find the subarray itself.
+Kadane's algorithm can be used to find the sum of the largest sum contiguous subarray or modified to find the subarray itself.
 
 Contributor's Note:
 This is a pretty easy algorithm to understand, although, seems a little scary by its name. I faced this problem in the interviews of companies like Walmart and Amazon.
