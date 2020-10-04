ccb0080ad7170ac7d0aad46751ec854e445e4711
Deepak Yadav <47855729+vieee@users.noreply.github.com>
2020-10-05 02:10:08 +0530

    Added reverse linkedlist implementation in C

---
+++ C/ReverseLinkedList.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Structure of a node */
+struct node {
+    int data; //Data part
+    struct node *next; //Address part
+}*head;
+
+
+/* Functions used in the program */
+void createList(int n);
+void reverseList();
+void displayList();
+
+
+int main()
+{
+    int n, choice;
+
+    /*
+     * Create a singly linked list of n nodes
+     */
+    printf("Enter the total number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
+
+    printf("\nData in the list \n");
+    displayList();
+
+    /*
+     * Reverse the list
+     */
+    printf("\nPress 1 to reverse the order of singly linked list\n");
+    scanf("%d", &choice);
+    if(choice == 1)
+    {
+        reverseList();
+    }
+
+    printf("\nData in the list\n");
+    displayList();
+
+    return 0;
+}
