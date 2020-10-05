801366b4715191ec17ff10318a19ab9c5dc23afc
Pushan Pore <68562005+pushanpore014@users.noreply.github.com>
2020-10-05 16:44:25 +0530

    Update sd_mean.py

--- Python/sd_mean.py
+++ Python/sd_mean.py
@@ -1,21 +1,18 @@
 import math
 def mean(l):
-    sum=0
-    for i in range(len(l)):
-        sum=sum+l[i]
-    return round(sum/len(l),2)
+    x=sum(l)
+    return round(x/len(l),2)
 
 def sqr(x):
     return x*x
 
 def sd(l):
-    s=0
+
     list1=list(map(sqr,l))
-    for i in range(len(list1)):
-        s=s+list1[i]
+    s=sum(list1)
     s=s/len(list1)-sqr(mean(l))
     return math.sqrt(s)
 
 l=[1,2,3,4,5]
-print("Mean: %.2f"%mean(l))
-print("Standard Deviation: %f"%round(sd(l),4))
+print(mean(l))
+print(round(sd(l),4))
