91440fbe0ddc4718d4cfc84e8d99f2426a4f97dd
syk007 <deepakumarvu@gmail.com>
2020-10-10 18:59:57 +0530

    Added Stack Operations in R

---
+++ R/StackOperations.R
@@ -0,0 +1,40 @@
+library(R6)
+Stack<-R6Class("Stack",list(
+        stack = c(),
+        push = function(x) {
+                self$stack <- append(self$stack,x)
+        },
+        pop = function() {
+                if(length(self$stack)>0) {
+                        top <- self$stack[1]
+                        self$stack <- self$stack[-c(1)]
+                        return(top)
+                }
+                else
+                        cat("Empty Stack")
+        },
+        display = function() {
+                print(self$stack)
+        },
+        size = function() {
+                cat("Number Of elements in stack are: ",length(self$stack))
+        }
+))
+
+# Some Basic Operations
+t<-Stack$new()
+t$push(1)
+t$push(array(c(2)))
+t$push("Element")
+t$display()
+t$size()
+t$pop()
+t$display()
+t$size()
+t$pop()
+t$display()
+t$size()
+t$pop()
+t$display()
+t$size()
+t$pop()
