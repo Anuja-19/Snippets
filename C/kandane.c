bb095480feb73e8019cfd68a34c17ef62a4dc505
Luvkumar8436 <luvkumar8436@gmail.com>
2020-10-14 09:28:17 +0530

    removing blank lines

--- C/kandane.c
+++ C/kandane.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-
 int main()
 {
 	//length of arr
 	int length;
-	scanf("%d",&length);
-	
+	scanf("%d",&length);	
 	int arr[length];
 	//enter array elements
 	for (int i = 0; i < length; i++)
 	{
 		scanf("%d",&arr[i]);
 	}
-
 	// max sum calculated so far
 	int max_sum_sofar = 0;
 	// max sum upto current element of arr
 	int max_sum_upto_here = 0;
-
 	for (int i = 0; i < length; i++)
 	{
 		max_sum_upto_here += arr[i];
@@ -28,11 +24,7 @@ int main()
 		if (max_sum_sofar < max_sum_upto_here)
 		{
 			max_sum_sofar = max_sum_upto_here ;
-		}
-		
-		
+		}	
 	}
 	printf("maximum subarray sum = %d\n" , max_sum_sofar);
-	
-	
 }
