5bbba1f76110efbafd90896f1c4e0bc6c1e85fa9
Bhoomchai L <bhoomchai.l@nomagic.com>
2020-10-27 10:56:03 +0700

    Added InsertionSort in Kotlin (Issue #43)

---
+++ Kotlin/InsertionSort.kt
@@ -0,0 +1,21 @@
+fun insertionSort(array: Array<Int>) {
+    for (i in array.indices) {
+        val tmp = array[i]
+        var j = i
+        while(j > 0 && array[j-1] > tmp){
+            array[j] = array[j-1]
+            j -= 1
+        }
+        array[j] = tmp
+    }
+}
+
+fun main() {
+    var input = arrayOf(42,27,5,7,0,15,2,99,1)
+    print("Original list: ")
+    input.iterator().forEach { print("$it ") }
+    println()
+    print("Sorted list: ")
+    insertionSort(input)
+    input.iterator().forEach { print("$it ") }
+}
